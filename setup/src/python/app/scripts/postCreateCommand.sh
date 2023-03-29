faa842309cd5c3fc3b1bfc62100ee320b120b4af
dennismeister93 <dennis.meister@bosch.com>
2023-03-29 09:20:56 +0200

    Adapt requirement files to follow best practice (#13)
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- setup/src/python/app/scripts/postCreateCommand.sh
+++ setup/src/python/app/scripts/postCreateCommand.sh
@@ -26,7 +26,7 @@ echo "### Install python requirements                     ###"
 echo "#######################################################"
 # Update pip before installing requirements
 pip3 install --upgrade pip
-REQUIREMENTS="./requirements-dev.txt"
+REQUIREMENTS="./requirements.txt"
 if [ -f $REQUIREMENTS ]; then
     pip3 install -r $REQUIREMENTS
 fi
@@ -41,7 +41,7 @@ if [ -f $REQUIREMENTS ]; then
 fi
 
 # Dependencies for unit and integration tests
-REQUIREMENTS="./app/tests/requirements-tests.txt"
+REQUIREMENTS="./app/tests/requirements.txt"
 if [ -f $REQUIREMENTS ]; then
     pip3 install -r $REQUIREMENTS
 fi
