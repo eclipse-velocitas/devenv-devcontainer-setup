d290e2323cbc4d70175ca347b0b7681faa5e842d
Dennis Meister <dennis.meister@bosch.com>
2023-08-28 14:40:59 +0200

    Try out integration in CI
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- setup/src/common/scripts/postStartCommand.sh
+++ setup/src/common/scripts/postStartCommand.sh
@@ -13,16 +13,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+set -e
+
 echo "#######################################################"
 echo "### Auto-Upgrade CLI                                ###"
 echo "#######################################################"
-
-# Check for internet connection
-wget -q --spider http://google.com
-if [ $? -ne 0 ]; then
-    echo "> No internet connection. Skipping Auto-Upgrade."
-    exit 0
-fi
 
 AUTHORIZATION_HEADER=""
 if [ "${GITHUB_API_TOKEN}" != "" ]; then
@@ -31,25 +26,32 @@ fi
 
 ROOT_DIRECTORY=$( realpath "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../.." )
 DESIRED_VERSION=$(cat $ROOT_DIRECTORY/.velocitas.json | jq .cliVersion | tr -d '"')
+
+if [ "$DESIRED_VERSION" = "latest" ]; then
+  CLI_RELEASES_URL=https://api.github.com/repos/eclipse-velocitas/cli/releases/latest
+else
+  CLI_RELEASES_URL=https://api.github.com/repos/eclipse-velocitas/cli/releases/tags/${DESIRED_VERSION}
+fi
+
 DESIRED_VERSION_TAG=$(curl -s -L \
 -H "Accept: application/vnd.github+json" \
 -H "X-GitHub-Api-Version: 2022-11-28" \
 ${AUTHORIZATION_HEADER} \
-https://api.github.com/repos/eclipse-velocitas/cli/releases/tags/${DESIRED_VERSION} | jq -r .name)
+${CLI_RELEASES_URL} | jq -r .name)
 
-if [ "$DESIRED_VERSION" != "$DESIRED_VERSION_TAG" ]; then
-  echo "> Can't find desired Velocitas CLI version: $DESIRED_VERSION"
-  exit 1
+if [ "$DESIRED_VERSION_TAG" = "null" ] || [ "$DESIRED_VERSION_TAG" = "" ]; then
+  echo "> Can't find desired Velocitas CLI version: $DESIRED_VERSION. Skipping Auto-Upgrade."
+  exit 0
 fi
 
 # Get installed version
 INSTALLED_VERSION=v$(velocitas --version | sed -E 's/velocitas-cli\/(\w+.\w+.\w+).*/\1/')
 
-if [ "$DESIRED_VERSION" != "$INSTALLED_VERSION" ]; then
+if [ "$DESIRED_VERSION_TAG" != "$INSTALLED_VERSION" ]; then
   echo "> Upgrading CLI..."
   CLI_ASSET_NAME=velocitas-linux-$(arch | sed 's/amd64/x64/g' | sed 's/aarch64/arm64/g')
   CLI_INSTALL_PATH=/usr/bin/velocitas
-  CLI_DOWNLOAD_URL="https://github.com/eclipse-velocitas/cli/releases/download/${DESIRED_VERSION}/${CLI_ASSET_NAME}"
+  CLI_DOWNLOAD_URL="https://github.com/eclipse-velocitas/cli/releases/download/${DESIRED_VERSION_TAG}/${CLI_ASSET_NAME}"
 
   echo "> Downloading Velocitas CLI from ${CLI_DOWNLOAD_URL}"
   sudo curl -s -L ${CLI_DOWNLOAD_URL} -o "${CLI_INSTALL_PATH}"
