3568bfea68d53d3d223d53848d469f2b9c1d032f
Dominic Sudy <dominic.sudy@bosch.com>
2023-09-21 10:51:56 +0200

    Fix CI

--- grpc-interface-support/src/cpp.py
+++ grpc-interface-support/src/cpp.py
@@ -67,7 +67,7 @@ class CppGrpcInterfaceGenerator(GrpcInterfaceGenerator):  # type: ignore
             get_package_path(), "grpc-interface-support", "templates", "cpp"
         )
 
-        deps_to_extract = ["grpc", "c-ares", "googleapis", "grpc-proto"]
+        deps_to_extract = ["grpc", "c-ares", "googleapis", "grpc-proto", "zlib"]
         deps_patterns = [
             re.compile(r"^.*\"(" + dep + r"\/.*)\".*$") for dep in deps_to_extract
         ]
