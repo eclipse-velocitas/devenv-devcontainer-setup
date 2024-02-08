fbbf8f39ec750483ce63a90278d6ece8482cbfbc
kse3hi <serhii.korchynskyi@etas.com>
2024-02-08 10:54:27 +0000

    Debug

--- sdk-installer/src/run.py
+++ sdk-installer/src/run.py
@@ -124,7 +124,6 @@ def main(verbose: bool):
 
     subprocess.check_call(
         ["git", "config", "--global", "--add", "safe.directory", sdk_install_path],
-        stdout=subprocess.DEVNULL if not verbose else None,
     )
 
     if lang == "cpp":
@@ -136,7 +135,6 @@ def main(verbose: bool):
     elif lang == "python":
         subprocess.check_call(
             ["python", "-m", "pip", "install", "."],
-            stdout=subprocess.DEVNULL if not verbose else None,
             cwd=sdk_install_path,
         )
 
