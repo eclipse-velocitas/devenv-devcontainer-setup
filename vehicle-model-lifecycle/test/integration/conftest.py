7d136bb24ef3e24fa071db2e5d251285e71a8fa4
Dominic Sudy <dominic.sudy@bosch.com>
2024-02-21 09:17:09 +0100

    Fix review findings

--- vehicle-model-lifecycle/test/integration/conftest.py
+++ vehicle-model-lifecycle/test/integration/conftest.py
@@ -24,11 +24,4 @@ def pytest_sessionstart(session):
 
     os.chdir(os.environ["VELOCITAS_TEST_ROOT"])
 
-    # FIXME: The C++ base image does not install conan globally
-    # but just for the vscode user, hence we have to download
-    # conan manually here. Can be removed once conan is installed
-    # globally.
-    if os.environ["VELOCITAS_TEST_LANGUAGE"] == "cpp":
-        subprocess.check_call(["python", "-m", "pip", "install", "conan==1.62.0"])
-
     subprocess.check_call(["velocitas", "init", "-f", "-v"], stdin=subprocess.PIPE)
