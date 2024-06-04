55a82b334fc7ad650658d72ca492d6f24dc9648e
Mark Hüsers <mark.huesers@etas.com>
2024-06-04 12:29:27 +0200

    Generate grpc service stubs
    To ensure a similar behavior as in c++ the python code generator for
    grpc stubs will create two files. One reflects the c++ header in the
    form of an abstract class and is always replaced when generating the
    SDK. The second one is the concrete child implementation and will only
    be generated if the file does not exist yet. This means that new
    method stubs are indicated for the developer inside the parent file
    so he can easily implement them.
    
    This behavior is not perfect. It would be best to also correctly
    update the child definitions, but this requires a bigger refactoring
    for the python and c++ grpc interface support generator.

--- grpc-interface-support/src/python.py
+++ grpc-interface-support/src/python.py
@@ -17,17 +17,21 @@ import os
 import shutil
 import subprocess
 from pathlib import Path
-from typing import List
+from typing import Dict, List
 
 import proto
 from generator import GrpcServiceSdkGenerator, GrpcServiceSdkGeneratorFactory
-from velocitas_lib.templates import CopySpec, copy_templates
-from velocitas_lib import (
-    get_package_path,
-    get_workspace_dir,
+from proto import ProtoFileHandle
+from shared_utils import (
+    capture_textfile_area,
     replace_in_file,
+    replace_line,
+    replace_text_area,
+    templates,
     to_camel_case,
 )
+from shared_utils.templates import CopySpec, copy_templates
+from velocitas_lib import get_package_path, get_workspace_dir
 
 
 def get_required_sdk_version_python() -> str:
@@ -42,7 +46,49 @@ def get_required_sdk_version_python() -> str:
     return sdk_version
 
 
+def get_template_dir() -> str:
+    return os.path.join(
+        get_package_path(), "grpc-interface-support", "data", "templates", "python"
+    )
+
+
+class GrpcCodeExtractor:
+    """
+    Provides methods for extracting code from generated gRPC python files.
+    """
+
+    def __init__(
+        self, proto_file: ProtoFileHandle, base_path: str, source_path: str = "."
+    ):
+        self.__proto_file = proto_file
+        self.__base_path = base_path
+        self.__source_path = source_path
+
+        self.grpc_source_path = os.path.join(
+            self.__base_path,
+            self.__source_path,
+            f"{self.__proto_file.get_service_name().lower()}_pb2_grpc.py",
+        )
+
+    def create_source_stub_code(self) -> List[str]:
+        service_name = to_camel_case(self.__proto_file.get_service_name())
+
+        source_content: List[str] = capture_textfile_area(
+            open(self.grpc_source_path, encoding="utf-8"),
+            f"class {service_name}Servicer(object):",
+            f"def add_{service_name}Servicer_to_server(servicer, server):",
+        )
+
+        # Remove leading whitespaces because they do not match the expected python intendation
+        if len(source_content) > 0:
+            source_content[0] = source_content[0].lstrip()
+
+        return source_content
+
+
 class PythonGrpcInterfaceGenerator(GrpcServiceSdkGenerator):  # type: ignore
+    TEMPLATE_PATH = "ServiceImpl.py"
+
     def __init__(
         self,
         package_directory_path: str,
@@ -52,6 +98,9 @@ class PythonGrpcInterfaceGenerator(GrpcServiceSdkGenerator):  # type: ignore
         self.__package_directory_path = package_directory_path
         self.__proto_file_handle = proto_file_handle
         self.__verbose = verbose
+        self.__seats_grpc_code_extractor = GrpcCodeExtractor(
+            self.__proto_file_handle, self.__package_directory_path, "seats_service_sdk"
+        )
 
     def __invoke_code_generator(self) -> None:
         subprocess.check_call(
@@ -138,6 +187,86 @@ class PythonGrpcInterfaceGenerator(GrpcServiceSdkGenerator):  # type: ignore
             template_dir, self.__package_directory_path, files_to_copy, variables
         )
 
+    def __create_stub_source(self) -> None:
+        app_source_dir = os.path.join(get_workspace_dir(), "app", "src")
+
+        source_code = self.__seats_grpc_code_extractor.create_source_stub_code()
+        variables = self.__create_stub_template_variables()
+        variables["service_source_code"] = "\n".join(source_code)
+
+        source_file_name = (
+            f"{self.__proto_file_handle.get_service_name()}ServiceStub.py"
+        )
+        source_file_path = os.path.join(app_source_dir, source_file_name)
+        templates.copy_templates(
+            get_template_dir(),
+            app_source_dir,
+            [templates.CopySpec(self.TEMPLATE_PATH, source_file_path)],
+            variables,
+        )
+
+    def __create_service_source(self) -> None:
+        app_source_dir = os.path.join(get_workspace_dir(), "app", "src")
+        service_source_file_name = (
+            f"{self.__proto_file_handle.get_service_name()}{self.TEMPLATE_PATH}"
+        )
+        service_source_file_path = os.path.join(
+            app_source_dir, service_source_file_name
+        )
+
+        if os.path.exists(service_source_file_path):
+            return
+
+        source_code = self.__seats_grpc_code_extractor.create_source_stub_code()
+        source_code = self.__transform_service_source_code(source_code)
+
+        variables = self.__create_service_template_variables()
+        variables["service_source_code"] = "\n".join(source_code)
+
+        templates.copy_templates(
+            get_template_dir(),
+            app_source_dir,
+            [templates.CopySpec("ServiceImpl.py", service_source_file_path)],
+            variables,
+        )
+
+    def __transform_service_source_code(self, lines: List[str]) -> List[str]:
+        source_content = replace_text_area(
+            lines,
+            '"""',
+            '"""',
+        )
+        source_content = replace_line(
+            source_content,
+            "context.set",
+        )
+
+        return source_content
+
+    def __create_stub_template_variables(self) -> Dict[str, str]:
+        service_name = self.__proto_file_handle.get_service_name()
+
+        return {
+            "service_name": service_name,
+            "service_name_parent_postfix": "Servicer",
+            "service_name_lower": service_name.lower(),
+            "service_name_camel_case": to_camel_case(service_name),
+            "service_name_postfix": "Stub",
+            "package_id": "seats_service_sdk.seats_pb2_grpc",
+        }
+
+    def __create_service_template_variables(self) -> Dict[str, str]:
+        service_name = self.__proto_file_handle.get_service_name()
+
+        return {
+            "service_name": "SeatsServiceStub",
+            "service_name_parent_postfix": "",
+            "service_name_lower": service_name.lower(),
+            "service_name_camel_case": to_camel_case(service_name),
+            "service_name_postfix": "",
+            "package_id": "SeatsServiceStub",
+        }
+
     def __install_module(self) -> None:
         subprocess.check_call(["pip", "install", self.__package_directory_path])
 
@@ -156,7 +285,8 @@ class PythonGrpcInterfaceGenerator(GrpcServiceSdkGenerator):  # type: ignore
         pass
 
     def update_auto_generated_code(self) -> None:
-        pass
+        self.__create_stub_source()
+        self.__create_service_source()
 
 
 class PythonGrpcServiceSdkGeneratorFactory(GrpcServiceSdkGeneratorFactory):  # type: ignore
