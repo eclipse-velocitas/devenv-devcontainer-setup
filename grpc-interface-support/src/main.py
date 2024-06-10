941d28b218d807cb0bc18cf80e7fde33a1937d74
dennismeister93 <dennis.meister@bosch.com>
2024-06-10 13:36:20 +0200

    Move shared utils to velocitas-lib (#71)
    * Move shared utils to velocitas-lib
    * Keep move_generated_sources
    * Use velocitas-lib v0.0.5
    ---------
    
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- grpc-interface-support/src/main.py
+++ grpc-interface-support/src/main.py
@@ -21,8 +21,12 @@ import proto
 from cpp import CppGrpcServiceSdkGeneratorFactory
 from generator import GrpcServiceSdkGeneratorFactory
 from python import PythonGrpcServiceSdkGeneratorFactory
-from shared_utils import create_truncated_string
-from velocitas_lib import download_file, get_programming_language, get_project_cache_dir
+from velocitas_lib import (
+    create_truncated_string,
+    download_file,
+    get_programming_language,
+    get_project_cache_dir,
+)
 from velocitas_lib.functional_interface import get_interfaces_for_type
 
 DEPENDENCY_TYPE_KEY = "grpc-interface"
