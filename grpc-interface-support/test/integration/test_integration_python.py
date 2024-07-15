0d37f8fa89dab717e2115b433b49fc048e8c3dde
Markus Petke <MP91@users.noreply.github.com>
2024-07-15 10:18:47 +0200

    test

--- grpc-interface-support/test/integration/test_integration_python.py
+++ grpc-interface-support/test/integration/test_integration_python.py
@@ -77,55 +77,17 @@ def assert_python_package_generated(service_name: str):
 
 
 def test_pip_package_is_usable():
-    from velocitas_sdk.base import Middleware, ServiceLocator
 
-    class TestClientServiceLocator(ServiceLocator):
-        def get_service_location(self, service_name: str) -> str:
-            return f"{service_name}@127.0.0.1:1234"  # noqa: E231
-
-        def get_metadata(self, service_name: Optional[str] = None):
-            pass
-
-    class TestServerServiceLocator(ServiceLocator):
-        def get_service_location(self, service_name: str) -> str:
-            return "127.0.0.1:1234"  # noqa: E231
-
-        def get_metadata(self, service_name: Optional[str] = None):
-            pass
-
-    class TestMiddleware(Middleware):
-        def __init__(self, serviceLocator: ServiceLocator) -> None:
-            self.service_locator = serviceLocator
-
-        async def start(self):
-            pass
-
-        async def wait_until_ready(self):
-            pass
-
-        async def stop(self):
-            pass
-
-    print("============= BUILDING SEATS SERVER ===================")
+    print("============= TEST SERVER ===================")
     os.chdir(os.environ["SERVICE_SERVER_ROOT"])
 
     assert subprocess.check_call(["velocitas", "init", "-v"]) == 0
 
     new_env = os.environ.copy()
     new_env["SDV_SEATS_ADDRESS"] = "127.0.0.1:1234"
+    new_env["SDV_HORNSERVICE_ADDRESS"] = "127.0.0.1:1234"
 
-    assert subprocess.check_call(["velocitas", "init", "-v"]) == 0
-
-    launcher = start_app("launcher_seats", new_env)
-
-    assert launcher.returncode == 0
-
-    print("============= TEST HORN SERVER ===================")
-    new_env["SDV_HORN_SERVICE_ADDRESS"] = "127.0.0.1:1234"
-
-    assert subprocess.check_call(["velocitas", "init", "-v"]) == 0
-
-    launcher = start_app("launcher_horn", new_env)
+    launcher = start_app("launcher", new_env)
 
     assert launcher.returncode == 0
 
--- test/python/service_client/app/src/launcher.py
+++ test/python/service_client/app/src/launcher.py
@@ -22,37 +22,8 @@ from seats_service_sdk.seats_pb2_grpc import (
     SeatsStub,
 )
 from seats_service_sdk.SeatsServiceClientFactory import SeatsServiceClientFactory
-from velocitas_sdk.base import Middleware, ServiceLocator
-
-
-class TestClientServiceLocator(ServiceLocator):
-    def get_service_location(self, service_name: str) -> str:
-        return f"{service_name}@127.0.0.1:1234"  # noqa: E231
-
-    def get_metadata(self, service_name: Optional[str] = None):
-        pass
-
-
-class TestServerServiceLocator(ServiceLocator):
-    def get_service_location(self, service_name: str) -> str:
-        return "127.0.0.1:1234"  # noqa: E231
-
-    def get_metadata(self, service_name: Optional[str] = None):
-        pass
-
-
-class TestMiddleware(Middleware):
-    def __init__(self, serviceLocator: ServiceLocator) -> None:
-        self.service_locator = serviceLocator
-
-    async def start(self):
-        pass
-
-    async def wait_until_ready(self):
-        pass
-
-    async def stop(self):
-        pass
+from velocitas_sdk.base import Middleware
+from velocitas_sdk import config
 
 
 def create_seats_client(middleware: Middleware) -> SeatsStub:
@@ -67,10 +38,6 @@ def create_horn_client(middleware: Middleware) -> HornServiceStub:
     return client
 
 
-SERVICE_ADRESS = "127.0.0.1:1234"
-
 if __name__ == "__main__":
-    middleware_client_mock = TestMiddleware(TestClientServiceLocator())
-
-    create_seats_client(middleware_client_mock)
-    create_horn_client(middleware_client_mock)
+    create_seats_client(config.middleware)
+    create_horn_client(config.middleware)
